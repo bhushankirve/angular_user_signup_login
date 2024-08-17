271da97db27ebb2b62516be8fd398dc3a9ecc1ad
Bhushan <bhushankirve1999@gmail.com>
2024-08-17 18:22:37 +0530

    Initial Commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+public loginForm !: FormGroup;
+constructor(private formBuilder: FormBuilder,
+  private http : HttpClient,
+  private router: Router
+
+) { }
+
+ngOnInit(): void {
+  this.loginForm = this.formBuilder.group ({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+}
+
+login()
+{
+  this.http.get<any>('http://localhost:3000/signupUsers')
+  .subscribe(res=> {
+    const user = res.find((a:any)=> {
+      return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+      });
+      if(user)
+        {
+          alert("Login Successful !!!");
+          this.loginForm.reset();
+          this.router.navigate(['dashboard']);
+        }
+        else{
+          alert("User not Found !!!");
+          }
+  }, err=>{
+    alert("Something went wrong !!!");
+  })
+}
+
+}
